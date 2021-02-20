5d408dd39b83b33170d8877392788a34ef73dfc3
mollnn <1084918073@qq.com>
2021-02-20 17:05:36 +0800

    feat 分离读取与渲染步骤

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,108 @@
 #include <bits/stdc++.h>
 #include <SDL2/SDL.h>
 #include "Bitmap.h"
+#include "image.hpp"
+#include "color.hpp"
 
-void startSDL(Bitmap& bitmap);
-void PutPixel24(SDL_Surface* surface, int x, int y, Uint32 color);
+void renderBMP(Bitmap &bitmap);
+void PutPixel24(SDL_Surface *surface, int x, int y, Uint32 color);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	if (argc != 2) return 1;
+	if (argc != 2)
+		return 1;
 	std::string filePath = argv[1];
 
 	Bitmap image(filePath);
-	if (image.isLoaded()) {
-		startSDL(image);
+	if (image.isLoaded())
+	{
+		renderBMP(image);
 	}
 
 	return 0;
 }
 
-void PutPixel24(SDL_Surface* surface, int x, int y, Uint32 color)
+void PutPixel24(SDL_Surface *surface, int x, int y, Uint32 color)
 {
-	Uint8* pixel = (Uint8*)surface->pixels;
+	Uint8 *pixel = (Uint8 *)surface->pixels;
 	pixel += (y * surface->pitch) + (x * sizeof(Uint8) * 4);
 
-	pixel[0] = (color >> 0) & 0xFF; //a
-	pixel[1] = (color >> 8) & 0xFF; //r
+	pixel[0] = (color >> 0) & 0xFF;	 //a
+	pixel[1] = (color >> 8) & 0xFF;	 //r
 	pixel[2] = (color >> 16) & 0xFF; //g
 	pixel[3] = (color >> 24) & 0xFF; //b
 }
 
-void startSDL(Bitmap& bitmap)
+Image createImageFromBMP(Bitmap &bitmap)
 {
-	BITMAP_INFO_HEADER& infoHeader = bitmap.getInfoHeader();
+	BITMAP_INFO_HEADER &infoHeader = bitmap.getInfoHeader();
+
+	int img_width = infoHeader.width;
+	int img_height = infoHeader.height;
+
+	//Load BMP
+	uint8 *pixels = bitmap.getPixels();
+
+	Image image(img_width, img_height);
+
+	// Read bitmap to image object
+	for (int y = 0; y < img_height; y++)
+	{
+		for (int x = 0; x < img_width; x++)
+		{
+			int i = x * 3 + (img_height - y - 1) * img_width * 3;
+			uint8 r = pixels[i + 2];
+			uint8 g = pixels[i + 1];
+			uint8 b = pixels[i + 0];
+			image.Set(x, y, colorUint8ToFloat(r, g, b));
+		}
+	}
+
+	return image;
+}
+
+void renderImage(Image &image)
+{
+	int img_width = image.size_x;
+	int img_height = image.size_y;
 
 	//init SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
 		return;
 	}
-	SDL_Window* window = SDL_CreateWindow("BMP Viewer", 100, 100, infoHeader.width, infoHeader.height, SDL_WINDOW_SHOWN);
-	if (!window) {
+	SDL_Window *window = SDL_CreateWindow("Image Viewer", 100, 100, img_width, img_height, SDL_WINDOW_SHOWN);
+	if (!window)
+	{
 		SDL_Quit();
 		return;
 	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (!renderer) {
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer)
+	{
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return;
 	}
 
 	//Create SW surface
-	SDL_Surface* surface = SDL_CreateRGBSurface(0, infoHeader.width, infoHeader.height, 24, 
-		0x0000FF00, 0x00FF0000, 0xFF000000, 0x00000000);
-	if (!surface) {
+	SDL_Surface *surface = SDL_CreateRGBSurface(0, img_width, img_height, 24,
+												0x0000FF00, 0x00FF0000, 0xFF000000, 0x00000000);
+	if (!surface)
+	{
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return;
 	}
 
-	//Load BMP
-	uint8* pixels = bitmap.getPixels();
 	SDL_LockSurface(surface);
-	for (int y = 0; y < infoHeader.height; y++) {
-		for (int x = 0; x < infoHeader.width; x++) {
-			int i = x*3 + (infoHeader.height-y-1)*infoHeader.width*3;
-			uint8 r = pixels[i+2];
-			uint8 g = pixels[i+1];
-			uint8 b = pixels[i+0];
-			//printf("%X,%X,%X ", pixels[i + 2], pixels[i + 1], pixels[i + 0]);
+	for (int y = 0; y < img_height; y++)
+	{
+		for (int x = 0; x < img_width; x++)
+		{
+			vec3 c = image.Get(x, y);
+			auto [r, g, b] = colorFloatToUint8(c);
 			uint32 color = SDL_MapRGB(surface->format, r, g, b);
 			PutPixel24(surface, x, y, color);
 		}
@@ -76,8 +110,9 @@ void startSDL(Bitmap& bitmap)
 	SDL_UnlockSurface(surface);
 
 	//Create HW surface
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-	if (!surface) {
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+	if (!surface)
+	{
 		SDL_FreeSurface(surface);
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
@@ -88,31 +123,40 @@ void startSDL(Bitmap& bitmap)
 
 	//Main loop
 	bool running = true;
-	while (running) {
+	while (running)
+	{
 		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if(event.type == SDL_QUIT) {
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
 				running = false;
 			}
 		}
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
-		
+
 		//Draw BMP
 		SDL_Rect dst;
 		dst.x = 0;
 		dst.y = 0;
-		dst.w = infoHeader.width;
-		dst.h = infoHeader.height;
+		dst.w = img_width;
+		dst.h = img_height;
 		SDL_RenderCopy(renderer, texture, NULL, &dst);
 
 		SDL_RenderPresent(renderer);
 		SDL_Delay(100);
 	}
-	
+
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+}
+
+void renderBMP(Bitmap &bitmap)
+{
+	Image image = createImageFromBMP(bitmap);
+	renderImage(image);
 }
